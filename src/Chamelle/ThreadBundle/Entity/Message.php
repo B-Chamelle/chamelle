19012f8d6da0b90f6846156f51b686a1ff916fcf
kvandel <>
2013-03-28 16:07:40 +0100

    Created ThreadBundle base model

---
+++ src/Chamelle/ThreadBundle/Entity/Message.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Chamelle\ThreadBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Message
+ *
+ * @ORM\Table(name="message")
+ * @ORM\Entity(repositoryClass="Chamelle\ThreadBundle\Entity\MessageRepository")
+ */
+class Message
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="msg_id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="msg_date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="msg_length", type="integer")
+     */
+    private $length;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="msg_body", type="text")
+     */
+    private $body;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Message
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set length
+     *
+     * @param integer $length
+     * @return Message
+     */
+    public function setLength($length)
+    {
+        $this->length = $length;
+    
+        return $this;
+    }
+
+    /**
+     * Get length
+     *
+     * @return integer 
+     */
+    public function getLength()
+    {
+        return $this->length;
+    }
+
+    /**
+     * Set body
+     *
+     * @param string $body
+     * @return Message
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+    
+        return $this;
+    }
+
+    /**
+     * Get body
+     *
+     * @return string 
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+}
