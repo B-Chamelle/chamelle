e1094bb68c7b37c3566889bd029327f1e0c8e733
kvandel <>
2013-04-03 15:51:05 +0200

    Edited templates and routes

--- src/Chamelle/ThreadBundle/Controller/ThreadController.php
+++ src/Chamelle/ThreadBundle/Controller/ThreadController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ThreadController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('ChamelleThreadBundle:Thread:index.html.twig', array('name' => $name));
+        return $this->render('ChamelleThreadBundle:Thread:index.html.twig');
     }
 }
