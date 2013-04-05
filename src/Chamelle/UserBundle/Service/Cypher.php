f2a2803fd05e1937f14857acda24c340f0cd78b0
kvandel <vandel.kevin@gmail.com>
2013-04-05 10:49:03 +0200

    Edited Cypher service

---
+++ src/Chamelle/UserBundle/Service/Cypher.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Chamelle\UserBundle\Service;
+
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
+
+class Cypher implements PasswordEncoderInterface
+{
+    const HASH_ALGORITHM = 'sha256';
+    const SALT_BYTES_LENGTH = 32;
+    
+    /**
+     * Adds salt then hashes the given password
+     *
+     * @param string $raw
+     * @param string $salt
+     */
+    public function encodePassword($raw, $salt)
+    {
+        return hash(self::HASH_ALGORITHM, $salt . $raw);
+    }
+    
+    
+    /**
+     * Checks if a raw password matches the given encoded password
+     *
+     * @param string $encoded
+     * @param string $raw
+     * @param string $salt
+     */
+    public function isPasswordValid($encoded, $raw, $salt)
+    {
+        return ($encoded === $this->encodePassword($raw, $salt));
+    }
+    
+    
+    /**
+     * Returns an hexadecimal random salt
+     */
+    public function generateSalt()
+    {
+        return bin2hex(mcrypt_create_iv(self::SALT_BYTES_LENGTH, MCRYPT_DEV_URANDOM));
+    }
+
+}
